98eb6280a19114b739c60087e46b4075625cac2b
Tebanqito <yemesesteban@gmail.com>
2023-06-18 16:45:22 -0300

    configuracion del servidor

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,20 @@
 import express, { Express } from "express";
-import { Server } from "socket.io";
+import socket from "socket.io";
 import router from "./routes/index";
-
-interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
-};
-
-interface ClientToServerEvents {
-  hello: () => void;
-};
-
-interface InterServerEvents {
-  ping: () => void;
-};
-
-interface SocketData {
-  name: string;
-  age: number;
-};
+import http, { Server } from "http";
 
 const app: Express = express();
+const server: Server = http.createServer(app);
 const port: number = 3000;
 
 app.use(express.json());
 
 app.use("/api", router);
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
 });
 
-export const io: Server = new Server<
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData
->(server);
+export const io: socket.Server = socket(server);
 
 export default app;
