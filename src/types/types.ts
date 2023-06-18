2bbaa142c4909e4884cace40996386940f3fc60b
Tebanqito <yemesesteban@gmail.com>
2023-06-18 03:28:09 -0300

    tipo Usuario y Room

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+type TipoMoneda = "Dolar" | "Peso" | "Real" | "Euro";
+
+interface PosicionMoneda {
+    x: number;
+    y: number;
+    z: number;
+}
+
+interface Moneda {
+    id: string;
+    tipoMoneda: TipoMoneda;
+    posicion: PosicionMoneda;
+}
+
+export interface Usuario {
+    id: string;
+    nombreUsuario: string;
+    monedas: Moneda[];
+    notificaciones: string[];
+};
+
+export interface Room {
+    id: string;
+    room: string;
+    monedas: Moneda[];
+};
