6585830e8ba7b08f589e40ae4f9203ae17a6c650
Tebanqito <yemesesteban@gmail.com>
2023-06-18 18:54:19 -0300

    GET /users

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,9 +1,25 @@
-import express, { Request, Response, Router } from 'express';
-import redis from 'redis';
-import { v4 as uuidv4 } from 'uuid';
-import { io } from '../app';
+import express, { Request, Response, Router } from "express";
+import redis, { RedisClient } from "redis";
+import { v4 as uuidv4 } from "uuid";
+import { io } from "../app";
+import { Usuario, Metaverso, Moneda, Room } from "../types/types";
 
-const userRouter = Router();
+const secret = { host: "", port: 3000, password: "" };
 
+const userRouter: Router = Router();
+const client: RedisClient = redis.createClient(secret);
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
+});
+
+userRouter.get("/users", async (req: Request, res: Response) => {
+  client.hgetall("users", (err: Error | null, users) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al obtener los usuarios" });
+    }
+    return res.json(users);
+  });
+});
 
 export default userRouter;
