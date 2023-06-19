40e8b157370a8dfdb4bb7126a052937af45c97c6
Tebanqito <yemesesteban@gmail.com>
2023-06-19 17:00:42 -0300

    PUT /api/rooms/update/:id

--- server-rest/src/routes/roomRouter.ts
+++ server-rest/src/routes/roomRouter.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import redis, { RedisClient } from "redis";
 import { Room, Moneda, LimitePoscion, Usuario } from "../types/types";
 import { v4 as uuidv4 } from "uuid";
+import { Console } from "console";
 
 const client: RedisClient = redis.createClient();
 const roomRouter: Router = Router();
@@ -33,30 +34,65 @@ roomRouter.get("/:id", (req: Request, res: Response) => {
 });
 
 roomRouter.post("/", (req: Request, res: Response) => {
-    const usuarios: Usuario[] = req.body.usuarios;
-    const limitesPosicion: LimitePoscion = req.body.limitePosicion;
-    const monedas: Moneda[] = req.body.monedas;
-    const nombreRoom: string = req.body.nombreRoom;
-    const cantidadMonedas: number = req.body.cantidadMonedas;
-    const id: string = uuidv4();
+  const usuarios: Usuario[] = req.body.usuarios;
+  const limitesPosicion: LimitePoscion = req.body.limitePosicion;
+  const monedas: Moneda[] = req.body.monedas;
+  const nombreRoom: string = req.body.nombreRoom;
+  const cantidadMonedas: number = req.body.cantidadMonedas;
+  const id: string = uuidv4();
 
-    const room: Room = {
-        id,
-        room: nombreRoom,
-        monedas,
-        limitesPosicion,
-        usuarios,
-        cantidadMonedas
+  const room: Room = {
+    id,
+    room: nombreRoom,
+    monedas,
+    limitesPosicion,
+    usuarios,
+    cantidadMonedas,
+  };
+
+  client.hset("rooms", JSON.stringify(room), (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: "Error al crear la room." });
     }
 
-    client.hset("rooms", JSON.stringify(room), (err) => {
+    res.status(200).json(room);
+  });
+});
+
+roomRouter.put("/update/:id", (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const attributes: Partial<Room> = req.body.attributes;
+
+  client.hget("rooms", id, (err: Error | null, room: string) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(400)
+        .json({
+          error: `Error al obtener la room con id ${id} para actualizar.`,
+        });
+    }
+
+    let roomToUpdate = JSON.parse(room);
+    roomToUpdate = { ...attributes };
+
+    client.hset(
+      "rooms",
+      id,
+      JSON.stringify(roomToUpdate),
+      (err: Error | null) => {
         if (err) {
-            console.log(err);
-            return res.status(400).json({ error: "Error al crear la room." });
+          console.log(err);
+          return res
+            .status(400)
+            .json({ error: `Error al actualizar la room con id ${id}.` });
         }
 
-        res.status(200).json(room);
-    });
+        res.status(200).json(roomToUpdate);
+      }
+    );
+  });
 });
 
 export default roomRouter;
