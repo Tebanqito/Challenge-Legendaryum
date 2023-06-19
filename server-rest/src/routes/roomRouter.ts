0513c64a7626919e6459c2fa9b9e836aa544ef14
Tebanqito <yemesesteban@gmail.com>
2023-06-19 16:21:23 -0300

    get para obtener las rooms

--- server-rest/src/routes/roomRouter.ts
+++ server-rest/src/routes/roomRouter.ts
@@ -1,9 +1,16 @@
-import { Router, Request, Response, NextFunction } from "express";
-import redis, { RedisClient } from "redis"
+import { Router, Request, Response } from "express";
+import redis, { RedisClient } from "redis";
+import { Room } from "../types/types";
 
 const client: RedisClient = redis.createClient();
 const roomRouter: Router = Router();
 
+roomRouter.get("/", (req: Request, res: Response) => {
+  client.hgetall("rooms", (err: Error | null, rooms) => {
+    if (err) return res.status(400).json({ error: err });
 
+    res.status(200).json(rooms);
+  });
+});
 
 export default roomRouter;
