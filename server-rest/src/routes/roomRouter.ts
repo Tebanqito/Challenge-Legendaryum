cfc42f9797ab9b44bdb08d3ce13b66e0ba36ba19
Tebanqito <yemesesteban@gmail.com>
2023-06-19 16:44:30 -0300

    POST /api/rooms

--- server-rest/src/routes/roomRouter.ts
+++ server-rest/src/routes/roomRouter.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import redis, { RedisClient } from "redis";
-import { Room } from "../types/types";
+import { Room, Moneda, LimitePoscion, Usuario } from "../types/types";
 import { v4 as uuidv4 } from "uuid";
 
 const client: RedisClient = redis.createClient();
@@ -30,6 +30,33 @@ roomRouter.get("/:id", (req: Request, res: Response) => {
 
     res.status(200).json(JSON.parse(room));
   });
+});
+
+roomRouter.post("/", (req: Request, res: Response) => {
+    const usuarios: Usuario[] = req.body.usuarios;
+    const limitesPosicion: LimitePoscion = req.body.limitePosicion;
+    const monedas: Moneda[] = req.body.monedas;
+    const nombreRoom: string = req.body.nombreRoom;
+    const cantidadMonedas: number = req.body.cantidadMonedas;
+    const id: string = uuidv4();
+
+    const room: Room = {
+        id,
+        room: nombreRoom,
+        monedas,
+        limitesPosicion,
+        usuarios,
+        cantidadMonedas
+    }
+
+    client.hset("rooms", JSON.stringify(room), (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: "Error al crear la room." });
+        }
+
+        res.status(200).json(room);
+    });
 });
 
 export default roomRouter;
