0c1ea071da85e008589f2e4295e3949fa62276b4
Tebanqito <yemesesteban@gmail.com>
2023-06-19 16:06:47 -0300

    app usando router principal

--- server-rest/src/app.ts
+++ server-rest/src/app.ts
@@ -1,9 +1,12 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
+import router from "./routes";
 
 const port: number = 3001;
 const app: Express = express();
 app.use(bodyParser.json());
+
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
