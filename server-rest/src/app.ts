09020b7c63b9738e4872e66b53e1cab8f33b659b
Tebanqito <yemesesteban@gmail.com>
2023-06-19 15:43:58 -0300

    app tipado

--- server-rest/src/app.ts
+++ server-rest/src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Express } from "express";
 import bodyParser from "body-parser";
 
 const port: number = 3001;
-const app = express();
+const app: Express = express();
 app.use(bodyParser.json());
 
 app.listen(port, () => {
