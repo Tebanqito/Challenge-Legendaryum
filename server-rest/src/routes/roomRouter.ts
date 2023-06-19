ec2862760b1a99a454ed5da119be678d3210725d
Tebanqito <yemesesteban@gmail.com>
2023-06-19 16:35:08 -0300

    GET /api/rooms/:id

--- server-rest/src/routes/roomRouter.ts
+++ server-rest/src/routes/roomRouter.ts
@@ -1,15 +1,34 @@
 import { Router, Request, Response } from "express";
 import redis, { RedisClient } from "redis";
 import { Room } from "../types/types";
+import { v4 as uuidv4 } from "uuid";
 
 const client: RedisClient = redis.createClient();
 const roomRouter: Router = Router();
 
 roomRouter.get("/", (req: Request, res: Response) => {
   client.hgetall("rooms", (err: Error | null, rooms) => {
-    if (err) return res.status(400).json({ error: err });
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: "Error al obtener las rooms." });
+    }
 
-    res.status(200).json(rooms);
+    res.status(200).json(JSON.parse(JSON.stringify(rooms)));
+  });
+});
+
+roomRouter.get("/:id", (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  client.hget("rooms", id, (err: Error | null, room: string) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(400)
+        .json({ error: `Error al obtener la room con el id ${id}.` });
+    }
+
+    res.status(200).json(JSON.parse(room));
   });
 });
 
