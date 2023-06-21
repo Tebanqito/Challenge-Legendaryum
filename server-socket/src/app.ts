fff25077e5ebe1a341cdf9a09db1285eca4126de
Tebanqito <yemesesteban@gmail.com>
2023-06-21 12:20:37 -0300

    seguro de obtencion de monedas y manejando las operaciones del cliente de forma asicronica en evento agarrarMoneda

--- server-socket/src/app.ts
+++ server-socket/src/app.ts
@@ -34,77 +34,176 @@ io.on("connection", (socket) => {
   });
 
   // USUARIO AGARRA UNA MONEDA -------------------------------------------------------------------------
+  // socket.on(
+  //   "agarrarMoneda",
+  //   (data: { idUsuario: string; idRoom: string; idMoneda: string }) => {
+  //     client.hget(
+  //       "users",
+  //       data.idUsuario,
+  //       (err: Error | null, userData: string) => {
+  //         if (err) return io.emit("error", err);
+
+  //         const user: Usuario = JSON.parse(userData); // obtengo el usuario
+
+  //         client.hget(
+  //           "rooms",
+  //           data.idRoom,
+  //           (err: Error | null, roomData: string) => {
+  //             if (err) return io.emit("error", err);
+
+  //             const room: Room = JSON.parse(roomData); // obtengo la room
+
+  //             let moneda: Moneda;
+  //             let tipoMoneda: TipoMoneda = "Dolar";
+  //             // recorro las monedas de la room para ver que moneda agarro el usuario
+  //             for (let index = 0; index < room.monedas.length; index++) {
+  //               if (room.monedas[index].id === data.idMoneda) {
+  //                 moneda = room.monedas[index];
+  //                 tipoMoneda = moneda.tipoMoneda;
+  //                 // saco de la room la moneda que agarro el usuario
+  //                 room.monedas.filter((m) => m.id !== moneda.id);
+  //                 user.monedas = [...user.monedas, moneda] as Moneda[]; // agrego la moneda al usuario
+  //                 break;
+  //               }
+  //             }
+
+  //             // actualizo el usuario con la nueva moneda que agarro
+  //             client.hset(
+  //               "users",
+  //               data.idUsuario,
+  //               JSON.stringify(user),
+  //               (err: Error | null) => {
+  //                 if (err) return io.emit("error", err);
+  //               }
+  //             );
+
+  //             // actualizo la room ya sin la moneda que agarro el usuario
+  //             client.hset(
+  //               "rooms",
+  //               data.idRoom,
+  //               JSON.stringify(room),
+  //               (err: Error | null) => {
+  //                 if (err) return io.emit("error", err);
+  //               }
+  //             );
+
+  //             // corroboro que el tipo de moneda que agarro el usuario siga en la room
+  //             const isTipoMoneda: boolean = room.monedas
+  //               .map((m) => m.tipoMoneda)
+  //               .includes(tipoMoneda);
+
+  //             // si ya no hay mas monedas de ese tipo en la room, entonces les aviso a todos los usuarios
+  //             if (!isTipoMoneda)
+  //               io.emit(
+  //                 "monedaNoDisponible",
+  //                 JSON.stringify({
+  //                   message: `La moneda ${tipoMoneda} ya no esta disponible en la room ${room.room}`,
+  //                 })
+  //               );
+  //           }
+  //         );
+  //       }
+  //     );
+  //     socket.emit("monedaObtenida", JSON.stringify({ message: "Mondea obtenida." }));
+  //   }
+  // );
+  
+  // CONTEMPLANDO EL CASO DE QUE DOS USAURIOS HAGAN UNA PETICION SOBRE UNA MISMA MONEDA AL MISMO TIEMPO 
+  // Y CONTEMPLANDO EL CASO DE QUE LA OPERACIONES DEL CLIENTE SON ASINCRONCAS --------------
+  // como las operaciones del cliente de redis son asincronicas entonces defino la funcion del segundo
+  // parametro con ASYNC para luego poder encargarme las operaciones del cliente con el AWAIT
   socket.on(
     "agarrarMoneda",
-    (data: { idUsuario: string; idRoom: string; idMoneda: string }) => {
-      client.hget(
-        "users",
-        data.idUsuario,
-        (err: Error | null, userData: string) => {
-          if (err) return io.emit("error", err);
+    async (data: { idUsuario: string; idRoom: string; idMoneda: string }) => {
+      try {
+        // trato de obtener la moneda y la operacion del cliente al tratar de obtener la moneda
+        // me retornara un valor false o true dependiendo si la moneda esta disponible o no
+        const isMoneda: boolean = await client.hget("monedas", data.idMoneda);
 
-          const user: Usuario = JSON.parse(userData); // obtengo el usuario
-
-          client.hget(
-            "rooms",
-            data.idRoom,
-            (err: Error | null, roomData: string) => {
-              if (err) return io.emit("error", err);
-
-              const room: Room = JSON.parse(roomData); // obtengo la room
-
-              let moneda: Moneda;
-              let tipoMoneda: TipoMoneda = "Dolar";
-              // recorro las monedas de la room para ver que moneda agarro el usuario
-              for (let index = 0; index < room.monedas.length; index++) {
-                if (room.monedas[index].id === data.idMoneda) {
-                  moneda = room.monedas[index];
-                  tipoMoneda = moneda.tipoMoneda;
-                  // saco de la room la moneda que agarro el usuario
-                  room.monedas.filter((m) => m.id !== moneda.id);
-                  user.monedas = [...user.monedas, moneda] as Moneda[]; // agrego la moneda al usuario
-                  break;
-                }
-              }
-
-              // actualizo el usuario con la nueva moneda que agarro
-              client.hset(
-                "users",
-                data.idUsuario,
-                JSON.stringify(user),
-                (err: Error | null) => {
-                  if (err) return io.emit("error", err);
-                }
-              );
-
-              // actualizo la room ya sin la moneda que agarro el usuario
-              client.hset(
-                "rooms",
-                data.idRoom,
-                JSON.stringify(room),
-                (err: Error | null) => {
-                  if (err) return io.emit("error", err);
-                }
-              );
-
-              // corroboro que el tipo de moneda que agarro el usuario siga en la room
-              const isTipoMoneda: boolean = room.monedas
-                .map((m) => m.tipoMoneda)
-                .includes(tipoMoneda);
-
-              // si ya no hay mas monedas de ese tipo en la room, entonces les aviso a todos los usuarios
-              if (!isTipoMoneda)
-                io.emit(
-                  "monedaNoDisponible",
-                  JSON.stringify({
-                    message: `La moneda ${tipoMoneda} ya no esta disponible en la room ${room.room}`,
-                  })
-                );
-            }
-          );
+        // si isMoneda es igual a false, entonces significa que la moneda no esta disponible,
+        // por lo tanto le aviso al usuario y me ahorro el proceso de obtener la room y el usuario
+        // antes que la moneda, ya que sin la moneda disponible seria en vano obtener el usuario y la room
+        if (!isMoneda) {
+          socket.emit("monedaNoDisponible", JSON.stringify({
+            message: "La moneda que intenta obtener ya no se encuentra disponible."
+          }));
+          throw new Error("Moneda no disponible.");
         }
-      );
-      socket.emit("monedaObtenida", JSON.stringify({ message: "Mondea obtenida." }));
+
+        // si isMoneda es igual true entonces significa que la moneda si esta disponible y puedo seguir
+        // con el sistema de que el usuario obtenga esa moneda
+
+        await client.hget(
+          "users",
+          data.idUsuario,
+          async (err: Error | null, userData: string) => {
+            if (err) return io.emit("error", err);
+
+            const user: Usuario = JSON.parse(userData); // obtengo el usuario
+
+            await client.hget(
+              "rooms",
+              data.idRoom,
+              async (err: Error | null, roomData: string) => {
+                if (err) return io.emit("error", err);
+
+                const room: Room = JSON.parse(roomData); // obtengo la room
+
+                let moneda: Moneda;
+                let tipoMoneda: TipoMoneda = "Dolar";
+                // recorro las monedas de la room para ver que moneda agarro el usuario
+                for (let index = 0; index < room.monedas.length; index++) {
+                  if (room.monedas[index].id === data.idMoneda) {
+                    moneda = room.monedas[index];
+                    tipoMoneda = moneda.tipoMoneda;
+                    // saco de la room la moneda que agarro el usuario
+                    room.monedas.filter((m) => m.id !== moneda.id);
+                    user.monedas = [...user.monedas, moneda] as Moneda[]; // agrego la moneda al usuario
+                    break;
+                  }
+                }
+
+                // actualizo el usuario con la nueva moneda que agarro
+                await client.hset(
+                  "users",
+                  data.idUsuario,
+                  JSON.stringify(user),
+                  (err: Error | null) => {
+                    if (err) return io.emit("error", err);
+                  }
+                );
+
+                // actualizo la room ya sin la moneda que agarro el usuario
+                await client.hset(
+                  "rooms",
+                  data.idRoom,
+                  JSON.stringify(room),
+                  (err: Error | null) => {
+                    if (err) return io.emit("error", err);
+                  }
+                );
+
+                // corroboro que el tipo de moneda que agarro el usuario siga en la room
+                const isTipoMoneda: boolean = room.monedas
+                  .map((m) => m.tipoMoneda)
+                  .includes(tipoMoneda);
+
+                // si ya no hay mas monedas de ese tipo en la room, entonces les aviso a todos los usuarios
+                if (!isTipoMoneda)
+                  io.emit(
+                    "monedaNoDisponible",
+                    JSON.stringify({
+                      message: `La moneda ${tipoMoneda} ya no esta disponible en la room ${room.room}`,
+                    })
+                  );
+              }
+            );
+          }
+        );
+        socket.emit("monedaObtenida", JSON.stringify({ message: "Mondea obtenida." }));
+      } catch(error) {
+        console.error(error);
+      }
     }
   );
 
